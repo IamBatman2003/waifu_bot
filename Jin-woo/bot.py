35f42d6fec55344ad3b3e01f3e4df9cad237b922
Ismith Ahd <162759069+IamBatman2003@users.noreply.github.com>
2025-05-05 15:31:15 +0530

    Update bot.py

--- Jin-woo/bot.py
+++ Jin-woo/bot.py
@@ -13,7 +13,7 @@ api_id_3 = 24906423
 api_hash_3 = '9823ed5cf0dac9192b183bf61eef4e0d'
 
 # === GROUP LINK ===
-group_link = 'https://t.me/+mtKgVsrcJg05ZmY1'
+group_link = 'https://t.me/+7_bRIW46wjNjNjll'
 
 # === AUTHORIZED USER IDS ===
 allowed_user_ids = [1745055042, 7280186793]
